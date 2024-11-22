cf5a1e286d45d597c25808f98e0c722d138e9adb
hishamabdalla <hishvmabdalla@gmail.com>
2024-11-22 20:49:06 +0200

    Product Module Entities

--- Store.API/Program.cs
+++ Store.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Store.Repository.Data.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<StoreDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
---
+++ Store.Repository/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.Data.Configurations
+{
+    public class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(p => p.PictureUrl)
+                .IsRequired();
+
+            builder.Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+
+            builder.HasOne(p => p.Brand)
+                .WithMany()
+                .HasForeignKey(p => p.BrandId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasOne(p => p.Type)
+           .WithMany()
+           .HasForeignKey(p => p.TypeId)
+           .OnDelete(DeleteBehavior.SetNull);
+
+            builder.Property(p => p.BrandId)
+               .IsRequired(false);
+
+            builder.Property(p => p.TypeId)
+               .IsRequired(false);
+        }
+    }
+}
