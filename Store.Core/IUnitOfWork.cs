f593afd1f47a9dc57c8836ba78017d4d66363148
hishamabdalla <hishvmabdalla@gmail.com>
2024-11-23 17:11:47 +0200

    Unit Of Work

---
+++ Store.Core/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Store.Core.Entities;
+using Store.Core.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Core
+{
+    public interface IUnitOfWork
+    {
+       Task<int> CompleteAsync();
+
+      IGenericRepository<TEntity,TKey> Repository<TEntity,TKey>() where TEntity :BaseEntity<TKey>;
+    }
+}
---
+++ Store.Repository/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using Store.Core;
+using Store.Core.Entities;
+using Store.Core.Repositories.Interfaces;
+using Store.Repository.Data.Contexts;
+using Store.Repository.Repositories;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreDbContext _context;
+        private Hashtable _repositories;
+        public UnitOfWork(StoreDbContext context)
+        {
+            _context = context;
+            _repositories = new Hashtable();
+        }
+        public async Task<int> CompleteAsync() => await _context.SaveChangesAsync();
+        
+
+        public IGenericRepository<TEntity, TKey> Repository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            if (!_repositories.ContainsKey(typeof(TEntity).Name))
+            {
+                var repository = new GenericRepository<TEntity, TKey>(_context);
+                _repositories.Add(typeof(TEntity).Name, repository);
+            }
+
+            return _repositories[typeof(TEntity).Name] as IGenericRepository<TEntity, TKey>;
+        }
+    }
+}
