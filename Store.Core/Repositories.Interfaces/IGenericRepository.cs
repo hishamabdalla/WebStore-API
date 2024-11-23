70a217a91ec7e53586e051dca21d14420782c13d
hishamabdalla <hishvmabdalla@gmail.com>
2024-11-23 11:21:37 +0200

    Generic Repository

---
+++ Store.Core/Repositories.Interfaces/IGenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Store.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Core.Repositories.Interfaces
+{
+    /// <summary>
+    /// Defines a generic repository for performing CRUD operations.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity being managed.</typeparam>
+    /// <typeparam name="TKey">The type of the entity's unique identifier.</typeparam>
+    public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        /// <summary>
+        /// Retrieves all entities asynchronously.
+        /// </summary>
+        /// <returns>A collection of all entities.</returns>
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        /// <summary>
+        /// Retrieves an entity by its unique identifier asynchronously.
+        /// </summary>
+        /// <param name="id">The identifier of the entity.</param>
+        /// <returns>The entity with the given identifier, or null if not found.</returns>
+        Task<TEntity> GetAsync(TKey id);
+
+        /// <summary>
+        /// Adds a new entity asynchronously.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        Task AddAsync(TEntity entity);
+
+        /// <summary>
+        /// Updates an existing entity.
+        /// </summary>
+        /// <param name="entity">The entity with updated values.</param>
+        void Update(TEntity entity);
+
+        /// <summary>
+        /// Removes an entity.
+        /// </summary>
+        /// <param name="entity">The entity to remove.</param>
+        void Delete(TEntity entity);
+    }
+
+}
---
+++ Store.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Core.Entities;
+using Store.Core.Repositories.Interfaces;
+using Store.Repository.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreDbContext _context;
+
+        public GenericRepository(StoreDbContext context)
+        {
+           _context = context;
+        }
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            if(typeof(TEntity)==typeof(Product))
+            {
+               return(IEnumerable<TEntity>) await _context.Products.Include(p=>p.Brand).Include(p=>p.Type).ToListAsync();
+            }
+           return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity> GetAsync(TKey id)
+        {
+            if (typeof(TEntity) == typeof(Product))
+            {
+                return await _context.Products.Include(p => p.Brand).Include(p => p.Type).FirstOrDefaultAsync(p=>p.Id==id as int?) as TEntity;
+            }
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.AddAsync(entity);
+
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Update(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+           _context.Remove(entity);  
+        }
+
+       
+    }
+}
