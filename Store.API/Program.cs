6b2da63abb7d4e6697a1865095838f6dee422bc2
hishamabdalla <hishvmabdalla@gmail.com>
2024-11-23 09:41:22 +0200

    Product Module - Data Seeding

--- Store.API/Program.cs
+++ Store.API/Program.cs
@@ -1,53 +1,63 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Store.Repository.Data;
 using Store.Repository.Data.Contexts;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddDbContext<StoreDbContext>(options =>
+namespace Store.API
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+            // Add services to the container.
 
-using var scope = app.Services.CreateScope();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-var services = scope.ServiceProvider;
 
-var context = services.GetRequiredService<StoreDbContext>();
-var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
-try
-{
-    context.Database.Migrate();
+            var app = builder.Build();
+
+            using var scope = app.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            var context = services.GetRequiredService<StoreDbContext>();
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                await context.Database.MigrateAsync();
+                await StoreDbContextSeed.SeedAsync(context);
+
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "There Are Problems During Apply Migrations !");
+            }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-catch (Exception ex)
-{
-    var logger= loggerFactory.CreateLogger<Program>();
-
-    logger.LogError(ex,"There are Problems during apply migration !");
-}
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ Store.Repository/Data/StoreDbContextSeed.cs
@@ -0,0 +1,68 @@
+﻿using Store.Core.Entities;
+using Store.Repository.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Store.Repository.Data
+{
+    public static class StoreDbContextSeed
+    {
+        public async static Task SeedAsync(StoreDbContext _context)
+        {
+            if (_context.Types.Count() == 0)
+            {
+                //1. Read Data From Json File
+                var typesData = File.ReadAllText(@"..\Store.Repository\Data\DataSeed\types.json");
+
+                //2. Convert Json String to List<T>
+                var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+
+                //3. Seed Data To DB
+                if (types is not null && types.Count() > 0)
+                {
+                    await _context.Types.AddRangeAsync(types);
+                    await _context.SaveChangesAsync();
+                }
+
+            }
+
+            if (_context.Brands.Count() == 0)
+            {
+                //1. Read Data From Json File
+                var brandsData = File.ReadAllText(@"..\Store.Repository\Data\DataSeed\brands.json");
+
+                //2. Convert Json String to List<T>
+                var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
+                //3. Seed Data To DB
+                if (brands is not null && brands.Count() > 0)
+                {
+                    await _context.Brands.AddRangeAsync(brands);
+                    await _context.SaveChangesAsync();
+                }
+
+            }
+
+            if (_context.Products.Count() == 0)
+            {
+                //1. Read Data From Json File
+                var productsData = File.ReadAllText(@"..\Store.Repository\Data\DataSeed\products.json");
+
+                //2. Convert Json String to List<T>
+                var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                //3. Seed Data To DB
+                if (products is not null && products.Count() > 0)
+                {
+                    await _context.Products.AddRangeAsync(products);
+                    await _context.SaveChangesAsync();
+                }
+
+            }
+        }
+    }
+}
