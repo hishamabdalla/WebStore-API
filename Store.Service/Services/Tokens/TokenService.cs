272717245210d22c65828eef1a78fe9bf283452d
hishamabdalla <hishvmabdalla@gmail.com>
2024-12-04 23:43:31 +0200

    Token Service

---
+++ Store.Service/Services/Tokens/TokenService.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Store.Core.Entities.Identity;
+using Store.Core.Services.Contract;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Service.Services.Tokens
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public async Task<string> CreateTokenAsync(AppUser user,UserManager<AppUser> userManager)
+        {
+            // 1. Header (algo, Type)
+            // 2. Payload (claims)
+            // 3. Signature ()
+
+            var claims = new List<Claim>()
+             {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.MobilePhone, user.PhoneNumber)
+             };
+
+            var userRoles =await userManager.GetRolesAsync(user);
+
+            foreach (var role in userRoles)
+            {
+                claims.Add(new Claim( ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["Jwt:Issuer"],
+                audience: configuration["Jwt:Audience"],
+                expires: DateTime.Now.AddDays(double.Parse(configuration["Jwt:DurationInDays"])),
+                claims: claims,
+                signingCredentials: creds
+
+             );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
