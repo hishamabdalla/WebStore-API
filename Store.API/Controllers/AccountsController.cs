3027036f1331111d8158dfba929680e930352eb1
hishamabdalla <hishvmabdalla@gmail.com>
2024-12-08 11:36:22 +0200

    Account Controller

---
+++ Store.API/Controllers/AccountsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.Core;
+using Store.Core.Dtos.Auth;
+
+namespace Store.API.Controllers
+{
+    public class AccountsController : BaseApiController
+    {
+        private readonly IUserService _userService;
+
+        public AccountsController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(LoginDto loginDto)
+        {
+            var user = await _userService.LoginAsync(loginDto);
+
+            if(user == null)
+            {
+                //error
+                return Unauthorized();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync(RegisterDto registerDto)
+        {
+            var user = await _userService.RegisterAsync(registerDto);
+
+            if (user == null)
+            {
+                //error
+                return BadRequest();
+            }
+
+            return Ok(user);
+        }
+    }
+}
