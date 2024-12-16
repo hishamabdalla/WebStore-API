8a23e7387aa73ce22dc51479241097ba3135dd8a
hishamabdalla <hishvmabdalla@gmail.com>
2024-12-16 17:16:29 +0200

    Update OrderService

---
+++ Store.Core/Specifications/Orders/OrderSpecificationWithPaymentIntentId.cs
@@ -0,0 +1,19 @@
+﻿using Store.Core.Entities.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Core.Specifications.Orders
+{
+    public class OrderSpecificationWithPaymentIntentId:BaseSpecifications<Order,int>
+    {
+        public OrderSpecificationWithPaymentIntentId(string paymentIntentId):base(o=>o.PaymentInternId==paymentIntentId) 
+        {
+            Include.Add(o => o.DeliveryMethod);
+            Include.Add(o => o.Items);
+
+        }
+    }
+}
--- Store.Service/Services/Orders/OrderService.cs
+++ Store.Service/Services/Orders/OrderService.cs
@@ -16,11 +16,13 @@ namespace Store.Service.Services.Orders
     {
         private readonly IUnitOfWork unitOfWork;
         private readonly IBasketRepository basketRepository;
+        private readonly IPaymentService paymentService;
 
-        public OrderService(IUnitOfWork unitOfWork,IBasketRepository basketRepository)
+        public OrderService(IUnitOfWork unitOfWork,IBasketRepository basketRepository,IPaymentService paymentService)
         {
             this.unitOfWork = unitOfWork;
             this.basketRepository = basketRepository;
+            this.paymentService = paymentService;
         }
 
       
@@ -49,7 +51,15 @@ namespace Store.Service.Services.Orders
            var deliveryMethod = await unitOfWork.Repository<DeliveryMethod, int>().GetAsync(deliveryMethodId);
 
             var subTotal=orderItems.Sum(i=>i.Price*i.Quantity);
-            var order = new Order(buyerEmail, shippingAddress, deliveryMethod, orderItems, subTotal, "");
+            if (!string.IsNullOrEmpty(basket.PaymentIntentId))
+            {
+                var spec = new OrderSpecificationWithPaymentIntentId(basket.PaymentIntentId);
+               var ExistOrder=await unitOfWork.Repository<Order, int>().GetWithSpecAsync(spec);
+                unitOfWork.Repository<Order,int>().Delete(ExistOrder);
+
+            }
+            var basketDto= await paymentService.SetPaymentIntentIdAsync(basketId);
+            var order = new Order(buyerEmail, shippingAddress, deliveryMethod, orderItems, subTotal, basketDto.PaymentIntentId );
 
            await unitOfWork.Repository<Order,int>().AddAsync(order);
            var result= await unitOfWork.CompleteAsync();
